19c185a902cddf54debc863431fd789103a24e1e
Ariel Martinez <arielstereo@msn.com>
2024-12-17 19:50:43 -0300

    fix fonts

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line camelcase
 import { Lexend, Archivo_Black } from 'next/font/google'
 
-const font = Lexend({ weight: '300' })
-const title = Archivo_Black({ weight: '400' })
+const font = Lexend({ weight: '300', subsets: ['latin'] })
+const title = Archivo_Black({ weight: '400', subsets: ['latin'] })
 
 export { font, title }
