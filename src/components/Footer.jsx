5b205b99358ff93b1d76ec5768ce3058b0bf77ad
Ariel Martinez <arielstereo@msn.com>
2024-12-19 19:37:15 -0300

    add portolio url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import LinkedinIcon from './icons/LinkedinIcon'
 
 const Footer = () => {
   return (
-    <div className='mt-32 w-full flex justify-center items-center'>
-      <Image src='/footerPng.png' width='600' height='600' alt='Footer-img' className='w-[800px] lg:w-[500px] h-full relative -bottom-6' />
-      <div className='z-10 absolute mt-24 lg:mt-20 flex gap-4'>
-        <Link href='https://www.linkedin.com/in/arielstereo/' target='_blank' className='text-base lg:text-xl text-white font-semibold hover:text-sky-400 pb-8' rel='noreferrer'>Ariel Martinez</Link>
-        <LinkedinIcon />
+    <div className='mt-32 w-full flex justify-center'>
+      <Image src='/footerPng.png' width='600' height='600' alt='Footer-img' className='w-full lg:w-[500px] h-full relative -bottom-24 xl:-bottom-48' />
+      <div className='z-10 absolute mt-32 lg:mt-20 xl:mt-60 flex gap-4'>
+        <Link href='https://portfolio.empren.dev/' target='_blank' className='text-base lg:text-xl text-white font-semibold hover:text-pink-200 pb-8' rel='noreferrer'>Ariel Martinez | 2024</Link>
       </div>
     </div>
   )
